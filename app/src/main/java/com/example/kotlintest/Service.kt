c5833ac9bd5e7986da5c6c0accc3b53cf52a4f5f
ilya veselov <ves.ilya@mail.ru>
2021-03-05 20:25:05 +0300

    Fix

--- app/src/main/java/com/example/kotlintest/Service.kt
+++ app/src/main/java/com/example/kotlintest/Service.kt
@@ -3,7 +3,17 @@ package com.example.kotlintest
 import kotlin.math.min
 
 class Service {
-    fun solve(input: Array<Array<Int>>, i: Int = 0, j: Int = 0): Int {
+    fun solve(input: Array<Array<Int>>): Int {
+        if (input.isEmpty()) {
+            return 0;
+        }
+        if (input.first().isEmpty()) {
+            return 0;
+        }
+        return solve(input, 0, 0);
+    }
+
+    private fun solve(input: Array<Array<Int>>, i: Int, j: Int): Int {
         if (i + 1 < input.size && j + 1 < input[0].size) {
             val result1 = solve(input, i + 1, j)
             val result2 = solve(input, i, j + 1)
--- app/src/test/java/com/example/kotlintest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/kotlintest/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.kotlintest
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.lang.Exception
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,42 +12,55 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun solve1() {
+    fun minimumPathSum_arrayWithEmptyArray() {
+        val input = arrayOf(arrayOf<Int>())
+        val output = Service().solve(input)
+        assertEquals(0, output)
+    }
+    @Test
+    fun minimumPathSum_emptyArray() {
+        val input = arrayOf<Array<Int>>()
+        val output = Service().solve(input)
+        assertEquals(0, output)
+    }
+
+    @Test
+    fun minimumPathSum_singleElement() {
         val input = arrayOf(arrayOf(1))
         val output = Service().solve(input)
         assertEquals(1, output)
     }
 
     @Test
-    fun solve2() {
+    fun minimumPathSum_severalElementsSquare() {
         val input = arrayOf(arrayOf(1,3,1),arrayOf(1,5,1),arrayOf(4,2,1))
         val output = Service().solve(input)
         assertEquals(7, output)
     }
 
     @Test
-    fun solve3() {
+    fun minimumPathSum_severalElementsRectangleHorizontal() {
         val input = arrayOf(arrayOf(1,2,3),arrayOf(4,5,6))
         val output = Service().solve(input)
         assertEquals(12, output)
     }
 
     @Test
-    fun solve4() {
+    fun minimumPathSum_severalElementsRectangleVertical() {
         val input = arrayOf(arrayOf(1,2),arrayOf(3,4),arrayOf(5,6))
         val output = Service().solve(input)
         assertEquals(13, output)
     }
 
     @Test
-    fun solve5() {
+    fun minimumPathSum_minElements() {
         val input = arrayOf(arrayOf(0,0),arrayOf(0,0))
         val output = Service().solve(input)
         assertEquals(0, output)
     }
 
     @Test
-    fun solve6() {
+    fun minimumPathSum_maxElements() {
         val input = arrayOf(arrayOf(100,100),arrayOf(100,100))
         val output = Service().solve(input)
         assertEquals(300, output)
